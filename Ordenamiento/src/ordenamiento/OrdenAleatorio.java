62e75853e22479842a1c8003dec3a38b1da7c596
Mario <>
2015-09-14 20:40:55 -0500

    -Orden Aleatorio

--- Ordenamiento/src/ordenamiento/OrdenAleatorio.java
+++ Ordenamiento/src/ordenamiento/OrdenAleatorio.java
@@ -1,4 +1,3 @@
-
 package ordenamiento;
 
 import java.util.Random;
@@ -8,10 +7,10 @@ public class OrdenAleatorio {
     public static void main(String[] args) {
         Ordenamiento o = new Ordenamiento();
         Pelicula[] peliculas, mergeSortArray, quickSortarray, insertionSortArray, bubbleSortArray;
-        long s, promTimeMerge = 0, promTimeInsertion = 0, auxTime, promTimeQuick=0, promTimeBubble=0;
+        long s, promTimeMerge = 0, promTimeInsertion = 0, auxTime, promTimeQuick = 0, promTimeBubble = 0;
         Random rndn = new Random();
-        int aux, auxCount, promCountInsertion = 0, promCountMerge = 0,promCountQuick=0,promCountBubble=0;
- 
+        int aux, auxCount, promCountInsertion = 0, promCountMerge = 0, promCountQuick = 0, promCountBubble = 0;
+
         for (int i = 0; i < 30; i++) {
             aux = rndn.nextInt(10500);
             peliculas = o.creaArrRndn(aux);
@@ -42,30 +41,30 @@ public class OrdenAleatorio {
 
             System.out.println("\nQuickSort.");
             s = System.currentTimeMillis();
-            auxCount=o.quickSort(quickSortarray, 0, quickSortarray.length - 1);
-            auxTime=(System.currentTimeMillis() - s);
-            promCountQuick=auxCount+promCountQuick;
-            promTimeQuick=auxTime+promTimeQuick;
+            auxCount = o.quickSort(quickSortarray, 0, quickSortarray.length - 1);
+            auxTime = (System.currentTimeMillis() - s);
+            promCountQuick = auxCount + promCountQuick;
+            promTimeQuick = auxTime + promTimeQuick;
             System.out.println("Número de Comparaciones: " + auxCount);
             System.out.println("Tiempo: " + auxTime + " ms. ");
-            
+
             System.out.println("\nBubbleSort.");
             s = System.currentTimeMillis();
-            auxCount=o.bubbleSort(bubbleSortArray);
-            auxTime=(System.currentTimeMillis() - s);
-            promCountBubble=auxCount+promCountBubble;
-            promTimeBubble=auxTime+promTimeBubble;
+            auxCount = o.bubbleSort(bubbleSortArray);
+            auxTime = (System.currentTimeMillis() - s);
+            promCountBubble = auxCount + promCountBubble;
+            promTimeBubble = auxTime + promTimeBubble;
             System.out.println("Número de Comparaciones: " + auxCount);
             System.out.println("Tiempo: " + auxTime + " ms. \n");
         }
-        System.out.println("Promedio Comparaciones InsertionSort: "+(promCountInsertion=promCountInsertion/30));
-        System.out.println("Promedio Comparaciones BubbleSort: "+(promCountBubble=promCountBubble/30));
-        System.out.println("Promedio Comparaciones MergeSort: "+(promCountMerge=promCountMerge/30));
-        System.out.println("Promedio Comparaciones QuickSort: "+(promCountQuick=promCountQuick/30));
-        System.out.println("Promedio Tiempo QuickSort: "+(promTimeQuick=promTimeQuick/30));
-        System.out.println("Promedio Tiempo BubbleSort: "+(promTimeBubble=promTimeBubble/30));
-        System.out.println("Promedio Tiempo MergeSort: "+(promTimeMerge=promTimeMerge/30));
-        System.out.println("Promedio Tiempo InsertionSort: "+(promTimeInsertion=promTimeInsertion/30));
-        
+        System.out.println("Promedio Comparaciones InsertionSort: " + (promCountInsertion = promCountInsertion / 30));
+        System.out.println("Promedio Comparaciones BubbleSort: " + (promCountBubble = promCountBubble / 30));
+        System.out.println("Promedio Comparaciones MergeSort: " + (promCountMerge = promCountMerge / 30));
+        System.out.println("Promedio Comparaciones QuickSort: " + (promCountQuick = promCountQuick / 30));
+        System.out.println("Promedio Tiempo QuickSort: " + (promTimeQuick = promTimeQuick / 30));
+        System.out.println("Promedio Tiempo BubbleSort: " + (promTimeBubble = promTimeBubble / 30));
+        System.out.println("Promedio Tiempo MergeSort: " + (promTimeMerge = promTimeMerge / 30));
+        System.out.println("Promedio Tiempo InsertionSort: " + (promTimeInsertion = promTimeInsertion / 30));
+
     }
 }
